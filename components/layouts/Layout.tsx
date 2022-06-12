bf2f6e4daab50fb5bee8eb0d537932cd1ca5e9c9
Daniel Correa <daniel.correa92@hotmail.com>
2022-06-12 17:09:32 -0500

    Agregamos una imagen que sirve para compartir

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -3,12 +3,16 @@ import { FC } from "react"
 import Head from "next/head"
 
 import {Navbar} from '../ui'
+import { useRouter } from 'next/router';
 
 interface Props{
   title?: String
 }
 
+const origin = (typeof window === 'undefined') ? '' : window.location.origin;
+
 const Layout: FC<Props> = ({children, title}) => {
+
   return (
     <>
         <Head>
@@ -16,6 +20,10 @@ const Layout: FC<Props> = ({children, title}) => {
             <meta name="author" content="Daniel Correa" />
             <meta name="description" content={`Informacíon sobre el pokemon : ${title}`}/>
             <meta name="keywords" content={`${title} pokemon, pokedex`}/>
+
+            <meta property="og:title" content={`Informacion Sobre el ${title}`} />
+            <meta property="og:description" content={`Esta es la pagina sobre ${title}`} />
+            <meta property="og:image" content={`${origin}/img/banner.png`} />
         </Head>
         
         <Navbar/>
